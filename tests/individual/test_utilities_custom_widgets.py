baeef99c95ba584e688d19e03cbcbd984a90f94e
Carl J. Nobile <carl.nobile@gmail.com>
2023-10-22 16:00:34 -0400

    Moved the individual tests.

--- tests/individual/test_utilities_custom_widgets.py
+++ tests/individual/test_utilities_custom_widgets.py
@@ -3,11 +3,7 @@
 # Test custom widgets.
 #
 
-import os
-import sys
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
+from .base_dir import *
 
 import wx
 
